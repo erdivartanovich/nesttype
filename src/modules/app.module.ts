0e55960c73dc5d6598bd131dbf63fe60d0000f0c
erdivartanovich <erdivartanovich@gmail.com>
2017-12-24 01:33:24 +0700

    add some comments for guideline

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './v1/user.controller';
 
+/**
+ * Application IOC container
+ * Register All working Controller in Controller key
+ * Register All components (services/ repository, factory etc in Components key)
+ */
 @Module({
   modules: [],
   controllers: [UserController],
