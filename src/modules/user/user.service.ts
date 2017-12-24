ad9760dbfa4ddacd6621634f9b05cabb033b25d3
erdivartanovich <erdivartanovich@gmail.com>
2017-12-24 22:55:31 +0700

    create user repository and service

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,17 @@
+import { Component, Inject } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User } from './user.entity';
+
+@Component()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+}
