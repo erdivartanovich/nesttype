7621244133925e1ed302116875878e1cb05d7734
erdivartanovich <erdivartanovich@gmail.com>
2018-01-18 21:55:34 +0700

    clean up debug code

--- src/commons/database.connection.ts
+++ src/commons/database.connection.ts
@@ -15,8 +15,6 @@ export async function databaseConnection() {
                 ]
             });
 
-            console.log(options);
-            
             return createConnection(options);
         })
 }
