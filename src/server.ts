1dd03a959c439b78ef2a02f9940371726948c059
erdivartanovich <erdivartanovich@gmail.com>
2018-01-20 23:10:33 +0700

    add jsonapi transformer interceptor

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './commons/app.module';
 import { JsonapiExceptionFilter } from './commons/exception/jsonapi-exception.filter';
 import { ValidationPipe } from './commons/pipe/validation.pipe';
+import { JsonapiTransformInterceptor } from './commons/transformer/jsonapi.transform.interceptor';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
 	app.useGlobalFilters(new JsonapiExceptionFilter());
 	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalInterceptors(new JsonapiTransformInterceptor());
 	await app.listen(3000);
 }
 bootstrap();
